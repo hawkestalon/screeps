bf03d18d70b487a278f9030f071460f126e91337
Zach Hawkes <hawkeye.zach@gmail.com>
2022-05-19 17:48:48 -0600

    harvester role

--- arrakis/roles/harvester.js
+++ arrakis/roles/harvester.js
@@ -3,9 +3,12 @@ const { transferEnergy } = require('../actions/transfer-energy');
 
 function run(creep) {
   const source = creep.pos.findClosestByPath(FIND_SOURCES);
-  const target = creep.room.find(structures)
+  const target = creep.room.find(structures, {
+    filter: (structure) => structure.type === STRUCTURE_SPAWN && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+  });
+  transferEnergy(creep, source, target);
 }
 
 module.exports = {
-  
+  run
 }
