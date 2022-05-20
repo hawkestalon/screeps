6352260198468d70aa0e4efa5b0e447680c65e37
Zach Hawkes <zach.hawkes@orbita.ai>
2022-05-20 13:49:44 -0600

    harvest role base behavior and strategy complete

---
+++ arrakis/actions.js
@@ -0,0 +1,33 @@
+
+
+function harvest(creep, source) {
+  if(creep.harvest(source, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+    creep.moveTo(source);
+  }
+}
+
+function transferEnergy(creep, target) {
+  if(creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+    creep.moveTo(target);
+  }
+}
+
+function upgrade(creep, target) {
+  if(creep.upgrade(target) === ERR_NOT_IN_RANGE) {
+    creep.moveTo(target);
+  }
+}
+
+function build(creep, target) {
+  if(creep.build(target) === ERR_NOT_IN_RANGE) {
+    creep.moveTo(target);
+  }
+}
+
+
+module.exports = {
+  harvest,
+  transferEnergy,
+  upgrade,
+  build,
+}
--- arrakis/harvester.js
+++ arrakis/harvester.js
@@ -1,16 +1,40 @@
 
-const { transferEnergy } = require('./transfer-energy');
+const actions = require('./transfer-energy');
 
 function run(creep) {
   const source = creep.pos.findClosestByPath(FIND_SOURCES);
-  const target = creep.room.find(FIND_STRUCTURES, {
+  const targets = creep.room.find(FIND_STRUCTURES, {
     filter: (structure) => structure.type === STRUCTURE_SPAWN && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
   });
+  runCoreBehavior(creep, source, targets);
+}
+
+function prioritizeStructure() {
+  // find the structure in most need of resources
+}
+
+function runStrategy(creep, strategy) {
+  const strategyFunction = STRATEGIES[strategy];
+  if(strategyFunction) {
+    return strategyFunction(creep);
+  }
+  return STRATEGIES.none(creep);
+}
+
+function runCoreBehavior(creep, source, targets) {
   if(creep.store.getFreeCapacity(RESOURCE_ENERGY) === 0) {
-    
+    actions.harvest(creep, source);
+  } else {
+    actions.transferEnergy(creep, targets[0]);
   }
 }
 
+const STRATEGIES = {
+  none: run,
+  harvest: run,
+}
+
 module.exports = {
-  run
+  run,
+  runStrategy
 }
